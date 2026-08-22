1430d40a9301b904eb34b99547edd645dbde5728
Tiaotiao <65841827@qq.com>
2026-08-22 16:23:19 +0800

    更新 DnsPacketHandler.java

--- app/src/main/java/com/tiao2/fastgithub/DnsPacketHandler.java
+++ app/src/main/java/com/tiao2/fastgithub/DnsPacketHandler.java
@@ -1,12 +1,19 @@
 package com.tiao2.fastgithub;
+
 import org.xbill.DNS.*;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
+
 public class DnsPacketHandler {
+
     public static String extractDomain(byte[] packet, int offset) {
         try {
-            Message query = new Message(packet, offset);
+            // 从偏移位置截取 DNS 消息
+            int len = packet.length - offset;
+            byte[] sub = new byte[len];
+            System.arraycopy(packet, offset, sub, 0, len);
+            Message query = new Message(sub);
             Record question = query.getQuestion();
             if (question != null) {
                 return question.getName().toString();
@@ -16,21 +23,31 @@ public class DnsPacketHandler {
         }
         return null;
     }
+
     public static byte[] buildAResponse(DatagramPacket queryPacket, int queryLen, String ip) {
         try {
-            Message query = new Message(queryPacket.getData(), queryPacket.getOffset());
+            byte[] data = queryPacket.getData();
+            int off = queryPacket.getOffset();
+            // 截取完整的 DNS 查询消息
+            byte[] sub = new byte[queryLen];
+            System.arraycopy(data, off, sub, 0, queryLen);
+            Message query = new Message(sub);
             Record question = query.getQuestion();
             if (question == null) return null;
+
             Name name = question.getName();
             int dnsClass = question.getDClass();
+
             Message response = new Message(query.getHeader().getID());
             response.getHeader().setFlag(Flags.QR);
             response.getHeader().setFlag(Flags.RA);
             response.getHeader().setRcode(Rcode.NOERROR);
             response.addRecord(question, Section.QUESTION);
+
             InetAddress addr = InetAddress.getByName(ip);
             Record answer = new ARecord(name, dnsClass, 300, addr);
             response.addRecord(answer, Section.ANSWER);
+
             return response.toWire();
         } catch (Exception e) {
             return null;
